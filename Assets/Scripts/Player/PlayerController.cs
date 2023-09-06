b50363ead75ba985c240f5d43ee5487a8d3a5d53
anastasia <anastasia@icecatgames.com>
2023-09-06 12:49:40 +0400

    added player walk animation, world borders with a follow camera

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,36 +7,36 @@ public class PlayerController : MonoBehaviour
     private Vector2 _movementInput;
 
     [SerializeField] private Rigidbody2D playerRigidBody;
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer playerSprite;
 
     [SerializeField] private float moveSpeed = 1f;
-    [SerializeField] private float collisionOffset = 0.05f;
-    [SerializeField] private ContactFilter2D movementFilter;
 
-    private List<RaycastHit2D> _castCollisions = new List<RaycastHit2D>();
-    
+    private static readonly int IsPlayerMoving = Animator.StringToHash("isMoving");
+
+    private bool _isMoving;
+
+    public bool IsMoving {
+        set
+        {
+            _isMoving = value;
+            animator.SetBool(IsPlayerMoving, _isMoving);
+        }
+    }
+
     private void FixedUpdate()
     {
         if (!_movementInput.Equals(Vector2.zero))
         {
-            var movedSuccessfully = TryMove(_movementInput);
-            if (!movedSuccessfully)
-                movedSuccessfully = TryMove(new Vector2(_movementInput.x, 0));
-            
-            if(!movedSuccessfully)
-                TryMove(new Vector2(0, _movementInput.y));
+            playerRigidBody.MovePosition(playerRigidBody.position + _movementInput.normalized * moveSpeed * Time.fixedDeltaTime );
+
+            playerSprite.flipX = _movementInput.x < 0 || (!(_movementInput.x > 0) && playerSprite.flipX);
+            IsMoving = true;
         }
-    }
-
-    private bool TryMove(Vector2 direction)
-    {
-        var collisionsCount = playerRigidBody.Cast(direction, movementFilter,
-            _castCollisions, moveSpeed * Time.deltaTime + collisionOffset);
-
-        if (collisionsCount != 0)
-            return false;
-        
-        playerRigidBody.MovePosition(playerRigidBody.position + direction *  moveSpeed * Time.deltaTime );
-        return true;
+        else
+        {
+            IsMoving = false;
+        }
     }
 
     public void OnMove(InputValue inputValue)
